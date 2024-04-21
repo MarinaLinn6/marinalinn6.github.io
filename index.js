73ec80df31b39b3c94e114c6c56c75cd49f78814
Marina Lin <qlin3628@usc.edu>
2024-04-20 22:14:56 -0700

    upda

--- index.js
+++ index.js
@@ -21,27 +21,38 @@ function scrollRotate() {
     image.style.transform = "rotate(" + window.pageYOffset / 8 + "deg)";
 }
 
-const containers = gsap.utils.toArray("#verticaltime");
-console.log(containers)
-containers.forEach((container) => {
-    let tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: container,
-            pin: false,
-            pinSpacing: false,
-            scrub: true,
-            markers: false
-        }
-    });
+// const containers = gsap.utils.toArray("#verticaltime");
+// console.log(containers)
+// containers.forEach((container) => {
+//     let tl = gsap.timeline({
+//         scrollTrigger: {
+//             trigger: container,
+//             pin: false,
+//             pinSpacing: false,
+//             scrub: true,
+//             markers: false
+//         }
+//     });
 
-    tl.to(container, {
-        autoAlpha: 1
-    }).to(
-        container,
-        {
-            autoAlpha: 0
-        },
-        0.5
-    );
+//     tl.to(container, {
+//         autoAlpha: 1
+//     }).to(
+//         container,
+//         {
+//             autoAlpha: 0
+//         },
+//         0.5
+//     );
+// });
+gsap.registerPlugin(ScrollTrigger);
+gsap.to("#verticaltime", {
+    scrollTrigger: {
+        trigger: "#verticaltime", // start the animation when ".box" enters the viewport (once)
+        toggleActions: "restart pause reverse pause",
+        scrub:true
+    },
+    y: 100,
+    // rotationt: 360,
+    duration:5,
 });
 
