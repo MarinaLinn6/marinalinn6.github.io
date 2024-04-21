3cb9b4c2f9f517267199b0e3fd18b1fac4b94075
Marina Lin <qlin3628@usc.edu>
2024-04-20 21:58:31 -0700

    upda

--- index.js
+++ index.js
@@ -20,3 +20,28 @@ function scrollRotate() {
     let image = document.getElementById("moon");
     image.style.transform = "rotate(" + window.pageYOffset / 8 + "deg)";
 }
+
+const containers = gsap.utils.toArray("#verticaltime");
+console.log(containers)
+containers.forEach((container) => {
+    let tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: container,
+            pin: false,
+            pinSpacing: false,
+            scrub: true,
+            markers: false
+        }
+    });
+
+    tl.to(container, {
+        autoAlpha: 1
+    }).to(
+        container,
+        {
+            autoAlpha: 0
+        },
+        0.5
+    );
+});
+
